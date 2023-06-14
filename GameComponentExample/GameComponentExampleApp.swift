117766a47fcebd35ad9e5b3ae59ce958574b34e4
Octavianus <oktagandajaya@gmail.com>
2023-06-14 16:36:18 +0700

    Initial Commit

---
+++ GameComponentExample/GameComponentExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameComponentExampleApp.swift
+//  GameComponentExample
+//
+//  Created by octavianus on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GameComponentExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
